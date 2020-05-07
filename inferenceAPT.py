cd0516f0327fe904c8f739319a9d369e362fa54f
Himanshu Ahuja <babahooja@gmail.com>
2020-05-06 21:35:35 -0400

    Added necessary files.

---
+++ inferenceAPT.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 23:10:25 2020
+
+@author: liushizhao
+"""
+
+
+#%% 
+import pickle
+import matlab.engine
+from delfi.simulator.BaseSimulator import BaseSimulator
+import os
+
+import delfi.distribution as dd
+import delfi.generator as dg
+import numpy as np
+import delfi.inference as infer
+from delfi.summarystats.BaseSummaryStats import BaseSummaryStats
+import copy
+from mat4py import loadmat
+import logging
+import datetime
+from scipy.io import savemat
+#%%
+def ConfirmationBiasSimulator(xval,fieldstofit,params, signals, engine):
+    # run matlab code run.vectorized and read results
+   
+
+    paramsNew = copy.deepcopy(params)
+    for i,f in enumerate(fieldstofit):
+        if f == 'samples':
+            paramsNew[f] = int(xval[i])
+        elif type(params[f]) is int:
+            paramsNew[f] = float(xval[i])
+
+
+    sim_results = engine.Model.runVectorizedAPT(paramsNew, signals.tolist())
+    sim_choice = np.squeeze(np.asarray(sim_results['choices']))
+
+
+
+    return sim_choice
+
+# define a simulator class linking to the real simulator
+class ConfirmationBias(BaseSimulator):
+    
+    def __init__(self, signals,fieldstofit,params,engine, seed = None):
+        
+        dim_param = 4
+        super().__init__(dim_param = dim_param,seed = seed)
+        self.signals = signals
+        self.simulate = ConfirmationBiasSimulator
+        self.engine = engine
+        self.fieldstofit = fieldstofit
+        self.params = params
+        
+    def gen_single(self,xval):
+        
+        choices = self.simulate(xval,self.fieldstofit,self.params,self.signals, self.engine)
+        return {'choices':choices}
+    
+class ConfirmationBiasStats(BaseSummaryStats):
+    
+    def __init__(self,signals,infoType,condition = 'choices_infoType',useStd = True):
+        self.signals = signals
+        self.infoType = infoType
+        self.condition = condition
+        self.useStd = useStd
+    def calc(self,repetition_list):
+
+        stats = []
+    
+        for r in range(len(repetition_list)):
+
+            data = repetition_list[r]
+            choices = np.asarray(data['choices'])
+            signals = np.asarray(self.signals)
+            infoType = np.asarray(self.infoType)
+            
+            if self.condition == 'choices_infoType':
+                # group trials according to both choices and catergory information (4 groups)
+                ind1 = np.where( (choices == 1) & (infoType == 1))[0]
+                ind2 = np.where( (choices == 1) & (infoType == -1))[0]
+                ind3 = np.where( (choices == -1) & (infoType == 1))[0]
+                ind4 = np.where( (choices == -1) & (infoType == -1))[0]
+                if self.useStd: 
+                    # include standard error mean in summary statistics
+                    sum_stats_vec = np.hstack((np.mean(signals[ind1,:],axis = 0),np.mean(signals[ind2,:],axis = 0),\
+                                               np.mean(signals[ind3,:],axis = 0),np.mean(signals[ind4,:],axis = 0),\
+                                               np.std(signals[ind1,:],axis = 0)/np.sqrt(len(ind1)),\
+                                               np.std(signals[ind2,:],axis = 0)/np.sqrt(len(ind2)),\
+                                               np.std(signals[ind3,:],axis = 0)/np.sqrt(len(ind3)),\
+                                               np.std(signals[ind4,:],axis = 0)/np.sqrt(len(ind4)))) 
+                else:
+                    # only use average of signals as summary statistics 
+                    sum_stats_vec = np.hstack((np.mean(signals[ind1,:],axis = 0),np.mean(signals[ind2,:],axis = 0),\
+                   np.mean(signals[ind3,:],axis = 0),np.mean(signals[ind4,:],axis = 0)))    
+            elif self.condition == 'choices':
+                # group trials according to just choices (2 groups)
+                ind1 = np.where(choices == 1)[0]
+                ind2 = np.where(choices == -1)[0]
+                if self.useStd:
+                    # include standard error mean in summary statistics
+                    sum_stats_vec = np.hstack((np.mean(signals[ind1,:],axis = 0),np.mean(signals[ind2,:],axis = 0),\
+                                               np.std(signals[ind1,:],axis = 0)/np.sqrt(len(ind1)),\
+                                               np.std(signals[ind2,:],axis = 0)/np.sqrt(len(ind2)))) 
+                else:
+                    # only use average of signals as summary statistics 
+                    sum_stats_vec = np.hstack((np.mean(signals[ind1,:],axis = 0),np.mean(signals[ind2,:],axis = 0),\
+                                              np.mean(np.std(signals[ind1,:],axis = 0)/np.sqrt(len(ind1))),\
+                                                  np.mean(np.std(signals[ind2,:],axis = 0)/np.sqrt(len(ind2)))))
+            elif self.condition == 'infoType':
+                # group trials according to just category information (2 groups)
+                ind1 = np.where(infoType == 1)[0]
+                ind2 = np.where(infoType == -1)[0]
+                if self.useStd:
+                    sum_stats_vec = np.hstack((np.mean(signals[ind1,:],axis = 0),np.mean(signals[ind2,:],axis = 0),\
+                                               np.std(signals[ind1,:],axis = 0)/np.sqrt(len(ind1)),\
+                                               np.std(signals[ind2,:],axis = 0)/np.sqrt(len(ind2)))) 
+                else:
+                    sum_stats_vec = np.hstack((np.mean(signals[ind1,:],axis = 0),np.mean(signals[ind2,:],axis = 0),\
+                                              np.mean(np.std(signals[ind1,:],axis = 0)/np.sqrt(len(ind1))),\
+                                                  np.mean(np.std(signals[ind2,:],axis = 0)/np.sqrt(len(ind2)))))
+                    
+                    
+            stats.append(sum_stats_vec)
+        return stats
+#%%
+# load the MATLAB engine
+if __name__ == "__main__":
+    
+    """"
+    variables to change for different inference tasks:
+    filename
+    fieldstofit and corresponding range
+    """
+    # create a log file
+    time_stamp = datetime.datetime.now()
+    currentTime  = time_stamp.strftime('%Y.%m.%d-%H:%M')
+    logname = 'inferenceAPT' + currentTime
+    logging.basicConfig(filename = logname,level = logging.DEBUG)
+    # create a savefolder
+    savefolder = '../dscData/resultsSyntheticDataCB/'
+    if not os.path.exists(savefolder):  
+        os.mkdir(savefolder)
+    
+    # set some hyperparameters
+    # training schedule
+    n_train = 5000
+    n_rounds = 3
+    # fitting setup
+
+    val_frac = 0.05
+    # network setup
+    
+    n_hiddens = [50,50]
+
+    # MAF parameters
+    density = 'mog'
+    n_mades = 5         # number of MADES
+    
+    hyperparameters = dict()
+    hyperparameters['n_train'] = n_train
+    hyperparameters['n_rounds'] = n_rounds
+    hyperparameters['val_frac'] = val_frac
+    hyperparameters['n_hiddens'] = n_hiddens
+    hyperparameters['density'] = density
+    hyperparameters['n_mades'] = n_mades
+    
+    # start matlab enigne and set path
+    engine = matlab.engine.start_matlab()
+    try:
+        ps = engine.genpath('/scratch/sliu88/APT')
+        engine.addpath(ps)
+    except:
+        pass
+    # save inference method settings (hyperparameters)
+    savemat(os.path.join(savefolder,'hyperparameters.mat'),{'hyperparameters':hyperparameters})
+    priorC_list = [1,2,3]
+    gamma_list = [1,2,3]
+    lapse_list = [1,2,3]
+    samples_list = [1,2,3]
+    for s in samples_list:
+        for l in lapse_list:
+            for g in gamma_list:
+                for C in priorC_list:
+                   
+                    # load data
+                    datafolder = '../dscData/syntheticDataCB'
+                    filename = 'Trial1priorC%dgamma%dlapse%dsamples%d' %(C,g,l,s)
+                    dataname = os.path.join(datafolder,filename+'.mat')
+                    savename = os.path.join(savefolder,'lshc_res'+ filename+'.mat')
+                    savenamepkl = os.path.join(savefolder,'lshc_res'+ filename+'.pkl')
+                    if not os.path.isfile(savename):
+                        logging.info('Running %dpriorC %dgamma %dlapse %dsamples' %(C,g,l,s))
+                        # load mat
+                        data = loadmat(dataname)
+                            
+                        lshc_choices = np.asarray(data['lshc_sim_results']['choices'])
+                        hslc_choices = np.asarray(data['hslc_sim_results']['choices'])
+                        lshc_signals = np.asarray(data['lshc_signals'])
+                        hslc_signals = np.asarray(data['hslc_signals'])
+                 
+                        lshc_params = data['lshc_params']
+                        hslc_params = data['hslc_params']
+                     
+                        lshc_infoType = np.ones(lshc_choices.shape)
+                        hslc_infoType = np.ones(hslc_choices.shape) * -1
+                        #%%
+                        # summary statistics of lshc condition
+                        lshc_s = ConfirmationBiasStats(lshc_signals,lshc_infoType,condition = 'choices',useStd = True)
+                        lshc_obs = {'choices':lshc_choices} 
+                        lshc_obs_stats = lshc_s.calc([lshc_obs])
+                        # summary statistics of hslc condition
+                        hslc_s = ConfirmationBiasStats(hslc_signals,hslc_infoType,condition = 'choices',useStd = True)
+                        hslc_obs = {'choices':hslc_choices}
+                        hslc_obs_stats = hslc_s.calc([hslc_obs])
+                        
+                    
+                        #%%
+                        
+                        
+                        # fieldstofit = ['prior_C','gamma','lapse','samples']
+                                           
+                        # prior_min = np.array([0,0,0,1])
+                        # prior_max = np.array([1,1,1,100])
+                       
+                        # seed_p = 2
+                        # prior =  dd.Uniform(lower = prior_min , upper = prior_max,seed = seed_p)
+                        # #%%
+                   
+                     
+                        # lshc_m = ConfirmationBias(lshc_signals,fieldstofit,lshc_params,engine)
+                        
+                        
+                        # gt = dg.Default(model=lshc_m, prior=prior, summary=lshc_s)
+                        
+                        # # set hyparameters 
+                        # # training schedule
+                        # n_train = 5000
+                        # n_rounds = 2
+                        # seed_inf = 1
+                        # pilot_samples = 2000
+                    
+                    
+                        # val_frac = 0.05
+                        # # network setup
+                        # n_hiddens = [50,50]
+                        # minibatch = 500
+                        # epochs = 100
+                        
+                        # prior_norm = True
+                        
+                        # # MAF parameters
+                        # density = 'mog'
+                        # n_mades = 5         # number of MADES
+    
+   
+   
+            
+                        # # inference object
+                        # res = infer.SNPEC(gt,
+                        #                 obs=lshc_obs_stats,
+                        #                 n_hiddens=n_hiddens,
+                        #                 seed=seed_inf,
+                        #                 pilot_samples=pilot_samples,
+                        #                 n_mades=n_mades,
+                        #                 prior_norm=prior_norm,
+                        #                 density=density)
+                        
+                        # # train
+                        # log, _, posterior = res.run(
+                        #                     n_train=n_train,
+                        #                     n_rounds=n_rounds,
+                        #                     minibatch=minibatch,
+                        #                 epochs=epochs,
+                        #                 silent_fail=False,
+                        #                 proposal='gaussian',
+                        #                 val_frac=val_frac,
+                        #                 verbose=True,)
+                    
+                        # # save posterior 
+                        # with open(savenamepkl,'wb') as f:
+                        #     pickle.dump([log,posterior],f)
+                        # # generate 2000 samples from posterior
+                        # posteriorSamples = []
+                        # for n in range(n_rounds):
+                        #     posteriorSamples.append(posterior[n].gen(2000))
+                        # # save these samples to a .matfile
+                    
+                   
+                    
+                    
+                        # savemat(savename,{'posterSamples':posteriorSamples,'log':log})
+                        # #logging.info('Finished %dpriorC %dgamma %dlapse %dsamples' %(C,g,l,s))
---
+++ mkFigureLineFitting.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 09:41:32 2020
+
+@author: liushizhao
+"""
+
+import numpy as np
+import pickle as pk
+import os
+import matplotlib.pyplot as plt
+from scipy import stats
+
+def calculate_discrepancy(samples1,samples2,option = 'parametric'):
+    if option == 'parametric':
+        nParams = samples1.shape[1]
+        nFeatures = 5
+        
+        discrepancy = np.zeros((nFeatures,1))
+        discrepancy[0] = abs(np.mean(samples1[:,0]) - np.mean(samples2[:,0]))
+        discrepancy[1] = abs(np.mean(samples1[:,1]) - np.mean(samples2[:,1]))
+        samples1_var = np.cov(samples1.T)
+        samples2_var = np.cov(samples2.T)
+        corr_samples1 = np.corrcoef(samples1[:,0],samples1[:,1])[0,1]
+        corr_samples2 = np.corrcoef(samples2[:,0],samples2[:,1])[0,1]
+        discrepancy[2] = abs(samples1_var[0,0] - samples2_var[0,0])
+        discrepancy[3] = abs(samples1_var[1,1] - samples2_var[1,1])
+        discrepancy[4] = abs(samples1_var[0,1] - samples2_var[0,1]) 
+        
+    elif option == 'nonparametric':
+        nParams = samples1.shape[1]  
+        discrepancy = np.zeros((nParams,1))
+        
+        for i in range(nParams):
+            discrepancy[i],_ = stats.ks_2samp(samples1[:,i],samples2[:,i])
+    return discrepancy.squeeze()
+# load analytical results
+name = 'LineFitting/analytical.pk'
+with open(name, 'rb') as handle:
+    file  = pk.load(handle)
+analytical_posterior = file[0]
+alpha_space = file[1]
+beta_space = file[2]
+# normalize the posterior, from log space to probabilty space summing up to 1
+analytical_posterior = np.exp(analytical_posterior)
+analytical_posterior = analytical_posterior /np.sum(analytical_posterior[:])
+# draw samples from analytical posterior
+# create a population of alph&beta
+alphav, betav = np.meshgrid(alpha_space , beta_space)
+
+k = 10000
+analytical_posterior_flat = analytical_posterior.flatten()
+index = np.random.choice(np.arange(len(analytical_posterior_flat)),size = k,replace = True,p = analytical_posterior_flat)
+analytical_samples = np.zeros((k,2))
+analytical_samples[:,0] = alphav.flatten()[index]
+analytical_samples[:,1] = betav.flatten()[index]
+#%%
+
+N_trial_list = [200,1000]
+samples_list = [2000,3000,5000,10000]
+
+
+    
+# nonpara_discrepancy_apt = np.zeros((len(samples_list),2,9))
+# nonpara_discrepancy_mh = np.zeros((len(samples_list),2,9))
+
+para_discrepancy_apt = np.zeros((2,len(samples_list),9,5))
+para_discrepancy_mh = np.zeros((2,len(samples_list),9,5))
+
+for i,N_trial in enumerate(N_trial_list):
+    for j,N in enumerate(samples_list):
+        if N_trial == 200:
+            resultsFolder = '../dscData/Line Fitting Results/Results-a/selfConsistencyOverParamDraws200'
+        elif N_trial == 1000:
+            resultsFolder = '../dscData/Line Fitting Results/Results-a/selfConsistencyOverParamDraws'
+
+        
+        
+        # load apt results
+        subfolder = os.path.join(resultsFolder,'%d_%d_2/'%(N_trial,N))
+        n1 = 0
+        n2 = 0
+        for file in os.listdir(subfolder):
+        
+            if file.endswith('.pk') & file.startswith('posterior_apt'):
+                with open(os.path.join(subfolder,file),'rb') as handle:
+                    res = pk.load(handle)
+    
+        
+                    n_rounds = len(file)
+                    posterior = res[-1]
+                    apt_samples = posterior.gen(k)
+                    
+                    #nonpara_discrepancy_apt[i,j,:,n1] = calculate_discrepancy(apt_samples,analytical_samples,option = 'nonparametric')
+                    para_discrepancy_apt[i,j,n1,:] = calculate_discrepancy(apt_samples,analytical_samples,option = 'parametric')
+                    n1 += 1                     
+            elif file.endswith('.pk') & file.startswith('mh_samples'):
+                with open(os.path.join(subfolder,file),'rb') as handle:
+                    res = pk.load(handle)
+                    mh_samples = res
+                    
+                    #nonpara_discrepancy_mh[i,j,:,n2] = calculate_discrepancy(mh_samples,analytical_samples,option = 'nonparametric')
+                    para_discrepancy_mh[i,j,n2,:] = calculate_discrepancy(mh_samples,analytical_samples,option = 'parametric')
+                    n2 += 1
+#%%  
+logpara_discrepancy_apt = np.log(para_discrepancy_apt)
+logpara_discrepancy_mh = np.log(para_discrepancy_mh)
+# from scipy.io import savemat
+# savemat('discrepancy',{})         
+# plt.figure()
+# feature_labels = ['alpha','beta']
+# for i in range(2):
+#     plt.subplot(2,1,i+1)
+#     plt.errorbar(np.log(np.asarray(samples_list)*2),y = np.mean(nonpara_discrepancy_apt[:,i,:],axis = 1),\
+#                  yerr = np.std(nonpara_discrepancy_apt[:,i,:],axis = 1))
+#     plt.errorbar(np.log(np.asarray(samples_list)*2),y = np.mean(nonpara_discrepancy_mh[:,i,:],axis = 1),\
+#                  yerr = np.std(nonpara_discrepancy_mh[:,i,:],axis = 1))
+#     plt.title(feature_labels[i])
+#     plt.xticks(np.log(np.asarray(samples_list)*2),['4000','6000','10000','20000'])
+#     if i == 0:
+#        plt.legend(['APT','mh_sampling'])
+fig,axs = plt.subplots(5,sharex = True,sharey = False)
+fig.set_size_inches(6, 10)
+fig.subplots_adjust(hspace=0.5, wspace=0.4)
+feature_labels = ['alpha_mean','beta_mean','alpha_variance','beta_variance','covariance']
+for i in range(5):
+
+    axs[i].errorbar(np.log(np.asarray(samples_list)*2),y = np.mean(logpara_discrepancy_apt[0,:,:,i],axis = 1),\
+                  yerr = np.std(logpara_discrepancy_apt[0,:,:,i],axis = 1),label ='apt_200',color = 'b',ls= '--')
+        
+    axs[i].errorbar(np.log(np.asarray(samples_list)*2),y = np.mean(logpara_discrepancy_mh[0,:,:,i],axis = 1),\
+                  yerr = np.std(logpara_discrepancy_mh[0,:,:,i],axis = 1),label = 'mh_sampling_200',color = 'g',ls = '--')
+    
+    axs[i].errorbar(np.log(np.asarray(samples_list)*2),y = np.mean(logpara_discrepancy_apt[1,:,:,i],axis = 1),\
+                  yerr = np.std(logpara_discrepancy_apt[1,:,:,i],axis = 1),label ='apt_1000',color = 'b')
+        
+    axs[i].errorbar(np.log(np.asarray(samples_list)*2),y = np.mean(logpara_discrepancy_mh[1,:,:,i],axis = 1),\
+                  yerr = np.std(logpara_discrepancy_mh[1,:,:,i],axis = 1),label = 'mh_sampling_1000',color='g')    
+    axs[i].set_title(feature_labels[i],size = 12)
+    axs[i].set_ylabel('Discrepancy')
+    plt.xticks(np.log(np.asarray(samples_list)*2),['4000','6000','10000','20000'])
+    if i == 4:
+        plt.legend(loc='l', bbox_to_anchor=(1,1))
+
+    plt.xlabel('Number of simulations',size = 16)
+     
+#plt.tight_layout()
